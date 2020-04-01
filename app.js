0a9fea1882497aaf6d8643549769f6aed602cb25
wuwei <1378823255@qq.com>
2020-04-01 09:15:15 +0800

    aby_tow

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use("/", function(req, res, next) {
 
 // 连接数据库
 mogonoose.connect('mongodb://127.0.0.1:27017/aby', function(err) {
-	if (!err) {
+	if (!err){
 		// 连接成功
 		app.use('/', router);
 		//中间件 app.use     
