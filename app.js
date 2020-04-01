b597830f020fe283f604b8d029af7dae84fdc8d5
wuwei <1378823255@qq.com>
2020-04-01 09:37:39 +0800

    aby_fous

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let path = require('path');
 app.use('/public',express.static(path.resolve(__dirname,'public')));
 
 app.use("/", function(req, res, next) {
+	
 	//设置允许跨域的域名，*代表允许任意域名跨域
 	res.header("Access-Control-Allow-Origin", "*");
 	
